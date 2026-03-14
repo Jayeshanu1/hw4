a6a1de0490305a4898dc50b2eb2a5bc8ca9533b5
Jayeshanu1 <jayesh.gautam@kellogg.northwestern.edu>
2026-03-14 23:23:31 +0000

    completed authorization

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,11 +1,15 @@
 class EntriesController < ApplicationController
 
-  def new
+def new
+  if current_user.nil?
+    redirect_to "/sessions/new"
   end
+end
 
   def create
     @entry = Entry.new
     @entry["title"] = params["title"]
+    @entry["user_id"] = current_user["id"]
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
