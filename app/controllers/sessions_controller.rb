93a5e34e8b4e0c19dca54beb8575e5d233d109f2
Jayeshanu1 <jayesh.gautam@kellogg.northwestern.edu>
2026-03-14 21:16:01 +0000

    On views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,20 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
+   def create
+    @user = User.find_by("email" => params["email"])
+    if @user.present? && @user.authenticate(params["password"])
+      session["user_id"] = @user.id
+      redirect_to "/"
+    else
+      redirect_to "/sessions/new"
+    end
   end
 
+
   def destroy
+    session["user_id"] = nil
+    redirect_to "/sessions/new"
   end
 end
   
